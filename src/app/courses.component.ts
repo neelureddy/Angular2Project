ad6e2168fe23ff4b764e822db300a1ac6ea10c90
neelureddy <neelureddy2000@gmail.com>
2021-09-23 17:09:19 -0400

    Update courses.component.ts
    event binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,13 +4,24 @@ import {Component} from '@angular/core';
 @Component ({
     selector: 'courses',
     template: `
-     <button [style.backgroundColor]="isActive ? 'blue': 'white' ">Save</button>
-    `
+    <div (click)= "onDivClicked()">
+    <button [style.backgroundColor]="isActive ? 'blue': 'white' ">Save</button>
+    <button (click)= "onSave($event)">Hit Click</button>
+    </div>`
 })
 export class CoursesComponent {
 
  constructor(){}
- isActive= false;
- 
+ isActive= true;
 
+ onDivClicked(){
+     console.log("Div was clicked");
+ }
+ 
+onSave($event: any){
+
+    $event.stopPropogation();
+    console.log("Button was clicked", $event);
+    }
 }
+
