b4c4ec0f04d914d53fe9ba8af49870521c3cc8e5
Neelima <45666209+neelureddy@users.noreply.github.com>
2021-09-24 20:44:46 -0400

    Event binding (#1)
    * pipes in angular
    
    * Custom pipe-summary pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
+import { SummaryPipe } from './summary.pipe';
 import { CoursesService } from './courses.service';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    SummaryPipe
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,24 +4,22 @@ import {Component} from '@angular/core';
 @Component ({
     selector: 'courses',
     template: `
-    <div (click)= "onDivClicked()">
-    <button [style.backgroundColor]="isActive ? 'blue': 'white' ">Save</button>
-    <button (click)= "onSave($event)">Hit Click</button>
-    </div>`
+    {{course.title | uppercase }} </br>
+    {{course.students| lowercase}}</br>
+    {{course.rating | number: '2.1-1'}}</br>
+    {{course.price | currency: AUD:true: '3.2-2'}}</br>
+    {{course.releaseDate| date: shortDate}}
+    {{text | summary: 10}}`
+
 })
 export class CoursesComponent {
-
- constructor(){}
- isActive= true;
-
- onDivClicked(){
-     console.log("Div was clicked");
- }
- 
-onSave($event: any){
-
-    $event.stopPropogation();
-    console.log("Button was clicked", $event);
+    course={
+        title: "The Complete Angular Course",
+        rating: 4.975,
+        students: 30123,
+        price: 190.95,
+        releasedate: new Date(2021, 09, 09)
     }
+ 
 }
 
